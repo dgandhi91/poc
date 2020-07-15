be0014c99a53a05a198789cf5d4f87c76469c758
degandhi <degandhi@redhat.com>
2020-07-15 21:55:49 +0530

    added pom version in auto tagging

--- autotagging1.sh
+++ autotagging1.sh
@@ -5,4 +5,5 @@ VERSION=`git describe master --tags --abbrev=0`
 echo "Latest Tag Version: $VERSION"
 
 #get current pom version
-echo "Pom Version: ${POM_VERSION}"
+POM_VERSION=mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['
+echo "Pom Version in auto tagging: $POM_VERSION"
