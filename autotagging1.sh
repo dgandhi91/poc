87dd30133e5df7fc75f7851b2c28a6f07a5f04e3
degandhi <degandhi@redhat.com>
2020-07-16 11:28:27 +0530

    added pom version in auto tagging

--- autotagging1.sh
+++ autotagging1.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
 #get highest tags across all branches, not just the current branch
-VERSION=`git describe --tags --abbrev=0`
-echo "Latest Tag Version: $VERSION"
+LATEST_TAG_VERSION=`git describe --tags --abbrev=0`
+echo "Latest Tag Version: LATEST_TAG_VERSION"
 
 #get current pom version
-echo "Pom Version in auto tagging: $1"
+POM_VERSION = $1
+echo "Pom Version in auto tagging: $POM_VERSION"
+
+if "$LATEST_TAG_VERSION" != "$POM_VERSION"; then
+    git tag "$POM_VERSION"
+    git push --tags
+    echo "Tag created and pushed: $POM_VERSION"
+else
+    echo "Already a tag on this commit"
+fi
+
+
